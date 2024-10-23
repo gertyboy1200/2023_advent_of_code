263dd9f06d3e4d27ddd6b421f7bafc733dcae9dd
gertyboy1200 <garrett.kolenbrander@gmail.com>
2024-10-23 15:31:00 -0600

    day_1_part_1🏁

---
+++ day_1.py
@@ -0,0 +1,20 @@
+with open('day_1_input.txt', 'r') as file:
+    lines = file.readlines()  # Reads all lines into a list
+
+output = []
+for line in lines:
+    
+# Extract the first and last digit only
+    first_digit = None
+    last_digit = None
+
+    for char in line:
+        if char.isdigit():
+            if first_digit is None:  # Store the first digit
+                first_digit = int(char)
+            last_digit = int(char)  # Continuously update the last digit
+    output.append(((first_digit * 10) + last_digit))
+
+print(sum(output))
+    
+   
